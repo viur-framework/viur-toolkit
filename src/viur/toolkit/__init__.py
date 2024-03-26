299203cc0705b3283141583f7254d23611d0b8e0
Sven Eberth <mail@sveneberth.de>
2024-03-26 13:50:41 +0100

    Import property

--- src/viur/toolkit/__init__.py
+++ src/viur/toolkit/__init__.py
@@ -6,7 +6,7 @@ from .db import *
 from .decorators import *
 from .memcache import *
 from .numeric import *
-# TODO: needs reimplementation from .property import *
+from .property import *
 # TODO: needs reimplementation from .report import *
 from .viur import *
 
