7b7c2a8432beba3e8e9360af7813a1e60f1fc2d4
Jan Max Meyer <jmm@phorward.de>
2024-07-16 15:28:55 +0200

    Updated __all__-variable

--- src/viur/toolkit/importer/__init__.py
+++ src/viur/toolkit/importer/__init__.py
@@ -4,3 +4,8 @@ Tools for copying data from ViUR 1.x to ViUR 3.x systems by using the JSON inter
 
 from .importable import Importable
 from .importer import Importer
+
+__all__ = [
+    "Importable",
+    "Importer",
+]
