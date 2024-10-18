62cd50ef01c91dbedc655d0ba28bf6cee8f13482
Sven Eberth <mail@sveneberth.de>
2024-10-19 01:40:37 +0200

    fix: Add missing `module` argument for `values_to_skel`
    Otherwise, `modify_skel_key` will never will be called.

--- src/viur/toolkit/importer/importable.py
+++ src/viur/toolkit/importer/importable.py
@@ -572,6 +572,7 @@ class Importable:
             source_key="key" if "key" in values else "id",  # ViUR 1.x
             enforce=enforce,
             debug=debug,
+            module=self,
         )
 
         if "outdated" in skel:
