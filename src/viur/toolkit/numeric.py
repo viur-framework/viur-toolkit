b754a69e74a0a5466f3a3edf8092559343efe54b
Sven Eberth <mail@sveneberth.de>
2025-07-28 15:43:47 +0200

    rename to thousands_separator

--- src/viur/toolkit/numeric.py
+++ src/viur/toolkit/numeric.py
@@ -12,28 +12,28 @@ def round_decimal(value: float, frac_digits: int = 2) -> float:
     return round(value * (10 ** frac_digits), 0) / (10 ** frac_digits)
 
 
-def format_number(value: float, frac_digits: int = 2, decimal_separator: str = "") -> str:
+def format_number(value: float, frac_digits: int = 2, thousands_separator: str = "") -> str:
     """
     Format a floating-point number with a specified number of fractional digits
-    and an optional custom decimal separator.
+    and an optional custom thousands separator.
 
     The function formats the given number using thousands separators and the specified
-    number of digits after the decimal point. If a custom decimal separator is provided,
-    it replaces the thousands separator in the integer part.
+    number of digits after the decimal point. If a custom thousands separator is provided,
+    it's used in the integer part.
 
     :param value: The number to format.
     :param frac_digits: Number of digits to display after the decimal point. Defaults to 2.
-    :param decimal_separator: Custom character to use as the thousands separator.
-                              If empty, no thousands separator is used.
+    :param thousands_separator: Custom character to use as the thousands separator.
+        If empty, no thousands separator is used.
     :return: The formatted number as a string.
     """
     format_str = "{{:,.{}f}}".format(frac_digits)
     number_str = format_str.format(value)
     if frac_digits > 0:
         before, after = number_str.split(".")
-        before = before.replace(",", decimal_separator)
+        before = before.replace(",", thousands_separator)
         return f"{before},{after}"
-    return number_str.replace(",", decimal_separator)
+    return number_str.replace(",", thousands_separator)
 
 
 format_currency = deprecated(  # type: ignore
